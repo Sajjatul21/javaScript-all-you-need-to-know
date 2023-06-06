7f7b91ae642b205b7707ff0c69a164c9a27963cd
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-06 16:10:07 +0100

    array destucturing
    আমরা array destucturing এর ক্ষেত্রে আমরা  যেগুলো পেতে না  চাই তার বেলায় আমরা কমা ব্যবহার করব

--- 147. Destructuring in Javascript/script.js
+++ 147. Destructuring in Javascript/script.js
@@ -10,3 +10,10 @@ let person = {
 console.log(name, email, address); */
 let { name, email, address: { city, country } } = person;
 console.log(city, country);
+
+
+// array decstructuring 
+
+let array = [1, 2, 3, 4, 5, 6, 7];
+let [first, second, , , , , last] = array;
+console.log(first, second, last);
