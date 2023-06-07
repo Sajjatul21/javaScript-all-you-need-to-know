39669ef8d87ed30ae621c3c2b0e05cd5c15ba874
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-07 08:56:24 +0100

    crate symbols

--- 148. Object From Entries in JavaScript/script.js
+++ 148. Object From Entries in JavaScript/script.js
@@ -3,3 +3,9 @@ const object = {
     b: 20
 };
 console.log(Object.entries(object));
+
+const objArr =[
+    ["a",10],
+    ["b",20]
+];
+console.log(Object.fromEntries(objArr))
---
+++ 149. Symbols in JavaScript/script.js
@@ -0,0 +1,4 @@
+let s1 = Symbol();
+let s2 = Symbol('test symbol');
+console.log(s1);
+console.log(s2);
