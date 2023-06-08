1ce5e1a84c171415bb94973dc14483bcec6b238b
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-08 17:21:33 +0100

    create iterator for array using es5

--- 151. Iterator Basic in Javascript/app.js
+++ 151. Iterator Basic in Javascript/app.js
@@ -1,6 +1,18 @@
-const arr = [1, 2, 3, 4];
+/* const arr = [1, 2, 3, 4];
 for (let i = 0; i < arr.length; i++) {
     const element = arr[i];
     console.log(element);
 
+} */
+
+function createIterator(collection) {
+    let index = 0;
+    return {
+        next() {
+            return {
+                done: i >= collection.length,
+                value: collection[i++]
+            };
+        }
+    };
 }
