376e6827562bd454f2ab07705fddf592b4ad8753
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-09 12:07:56 +0100

    yield মানে pose করা

--- 154. Generators in Javascript/app.js
+++ 154. Generators in Javascript/app.js
@@ -1,9 +1,26 @@
-function* generate() {
+/* function* generate() {
     yield 1;
     yield 2;
     yield 3;
-}
+} 
+
 let iterate = generate();
+console.log(iterate.next());
+console.log(iterate.next());
+console.log(iterate.next());
+console.log(iterate.next());
+console.log(iterate.next()); */
+
+
+
+function* generate(collection) {
+    for (let i = 0; i < collection.length; i++) {
+        yield collection[i];
+    }
+}
+
+const array = [11, 22, 33];
+let iterate = generate(array);
 console.log(iterate.next());
 console.log(iterate.next());
 console.log(iterate.next());
