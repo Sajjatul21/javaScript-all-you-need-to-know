373e4017deb9ee5a1323c0f29751aa3905a2d83c
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-09 16:04:41 +0100

    update / delete/ get the value

--- 156. Map in JavaScript/app.js
+++ 156. Map in JavaScript/app.js
@@ -3,4 +3,11 @@ let map = new Map([
     ["b", 20],
     ["c", 30],
 ]);
+// update value 
+map.set('d', 40);
+// delete value
+map.delete('b');
+// get the value
+console.log(map.get('a'));
 console.log(map);
+console.log(map.size);
