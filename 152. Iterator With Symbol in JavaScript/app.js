afa28f44f6f07b18592423c4000737ed72013e8d
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-09 11:18:00 +0100

    string iterator

--- 152. Iterator With Symbol in JavaScript/app.js
+++ 152. Iterator With Symbol in JavaScript/app.js
@@ -11,9 +11,20 @@ const arr = [1, 2, 3];
     };
 }
  */
-let iterate = arr[Symbol.iterator]()
+let iterate = arr[Symbol.iterator]();
 
 console.log(iterate.next());
 console.log(iterate.next());
 console.log(iterate.next());
 console.log(iterate.next());
+
+
+const str = "TEXT";
+let iterateText = str[Symbol.iterator]();
+
+console.log(iterateText.next());
+console.log(iterateText.next());
+console.log(iterateText.next());
+console.log(iterateText.next());
+console.log(iterateText.next());
+console.log(iterateText.next());
