e74121e3ad202c939574d3d90b3ddaba27c7ce3e
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-09 16:09:37 +0100

    for of on map. chack is it iterable

--- 156. Map in JavaScript/app.js
+++ 156. Map in JavaScript/app.js
@@ -8,10 +8,15 @@ map.set('d', 40);
 // delete value
 map.delete('b');
 // get the value
-console.log(map.get('a'));
+/* console.log(map.get('a'));
 console.log(map);
 console.log(map.size);
 
 console.log(map.values());
 console.log(map.keys());
-console.log(map.entries())
+console.log(map.entries()) */
+
+
+for (let [k, v] of map) {
+    console.log(k, v);
+}
