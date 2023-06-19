44285992607a4a16af385c9d0b410108b66acc74
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-19 15:23:40 +0100

    make a class

--- 159. Class in JavaScript/app.js
+++ 159. Class in JavaScript/app.js
@@ -7,3 +7,15 @@ Reactangle.prototype.draw = function () {
     console.log('Drawing....');
 };
 let react1 = new Reactangle(12, 15);  */
+
+class Reactangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    draw() {
+        console.log('Drawing.......');
+    }
+}
+const react1 = new Reactangle(23, 55);
+console.log(react1);
