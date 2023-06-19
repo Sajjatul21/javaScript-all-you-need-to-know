d79d6de1e93204937bb3ed31a44e1a66d20461f9
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-06-19 15:18:54 +0100

    আমরা এই কোড কে class এর রাুপান্তর করব

---
+++ 159. Class in JavaScript/app.js
@@ -0,0 +1,9 @@
+/* function Reactangle(width, height) {
+    this.width = width;
+    this.height = height;
+}
+Reactangle.prototype.draw = function () {
+    console.log(this);
+    console.log('Drawing....');
+};
+let react1 = new Reactangle(12, 15);  */
