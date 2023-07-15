d2746f905999c32c02799160fe7495f8ad654fae
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-15 09:33:07 -0700

    through error. when through error the  code go on catch section

--- 172. Throwing an Error in JavaScript/app.js
+++ 172. Throwing an Error in JavaScript/app.js
@@ -1,6 +1,7 @@
 try {
     console.log('I am line 1');
     console.log('I am line 2');
+    throw new Error('I am your big big big big Error');
     console.log('I am line 3');
     console.log('I am line 4');
 }
