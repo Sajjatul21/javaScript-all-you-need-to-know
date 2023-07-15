c1a704c0e2f778d5723b9d24d84cf883589c892a
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-14 23:39:12 -0700

    static method লেখার জন্য আমাদের
    static লিখতে হয়

--- 161. Static Method in JavaScript/app.js
+++ 161. Static Method in JavaScript/app.js
@@ -8,7 +8,7 @@ Reactangle.prototype.draw = function () {
 };
 let react1 = new Reactangle(12, 15);  */
 
-class Reactangle {
+/* class Reactangle {
     constructor(width, height) {
         this.width = width;
         this.height = height;
@@ -19,6 +19,25 @@ class Reactangle {
 }
 const react1 = new Reactangle(23, 55);
 console.log(react1);
-// 
+ */
 
+class Person {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+    print() {
+        console.log(this.name, this.email);
+    }
+    static create(str) {
+        let person = JSON.parse(str);
+        return new Person(person.name, person.email);
+    }
+}
 
+let str = '{"name":"Sajjatul Islam","email":"sajjatul@example.com"}';
+
+let p1 = Person.create(str);
+console.log(p1);
+console.log(p1 instanceof Person);
+p1.print();
