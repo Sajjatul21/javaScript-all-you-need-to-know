e50eee739ab768ef8c198a67ba569de4db81d572
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-14 23:14:54 -0700

    যে সব method ব্যবহার করতে আমাদের
    class এর  instance  তৈরি করতে হয় না new  এই কিওয়ার্ড দিয়ে আমাদের object make করতে হয় না directly সেই সব মেথডে ‍access নেয়া যায়ে সেই মেথড গুলাকে বলা হয় static method ।

---
+++ 161. Static Method in JavaScript/app.js
@@ -0,0 +1,24 @@
+/* function Reactangle(width, height) {
+    this.width = width;
+    this.height = height;
+}
+Reactangle.prototype.draw = function () {
+    console.log(this);
+    console.log('Drawing....');
+};
+let react1 = new Reactangle(12, 15);  */
+
+class Reactangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    draw() {
+        console.log('Drawing.......');
+    }
+}
+const react1 = new Reactangle(23, 55);
+console.log(react1);
+// 
+
+
