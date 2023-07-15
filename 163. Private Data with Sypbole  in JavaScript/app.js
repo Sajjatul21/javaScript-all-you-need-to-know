51828fbadfa3ec76c4ed466205eaca57d6350cc1
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-15 00:26:28 -0700

    এখন আমরা সহযে reduce method টাকে পাওয়া যাবে না

--- 163. Private Data with Sypbole  in JavaScript/app.js
+++ 163. Private Data with Sypbole  in JavaScript/app.js
@@ -1,10 +1,11 @@
+const _radius = Symbol();  // symbol make unique identyfire
 class Circle {
     constructor(radius) {
-        this.radius = radius;
+        this[_radius] = radius;
     }
     draw() {
         console.log('Drawing.....');
     }
 }
-let c1 = new Circle(2)
-c1.
+let c1 = new Circle(2);
+console.log(c1._radius);
