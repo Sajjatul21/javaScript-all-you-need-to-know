4e4b52b3cf9bfc8b9776cfedb58d5f7e7ee6219b
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-15 00:33:07 -0700

    আমরা  Object.getOwnPropertySymbols দিয়ে method গুলো access  করতে  পারি

--- 163. Private Data with Sypbole  in JavaScript/app.js
+++ 163. Private Data with Sypbole  in JavaScript/app.js
@@ -1,11 +1,18 @@
 const _radius = Symbol();  // symbol make unique identyfire
+const _name = Symbol();
 class Circle {
     constructor(radius) {
         this[_radius] = radius;
+        this[_name] = name;
     }
     draw() {
         console.log('Drawing.....');
     }
 }
-let c1 = new Circle(2);
-console.log(c1._radius);
+let c1 = new Circle(2, 'shihab');
+console.log(c1);
+
+console.log(Object.getOwnPropertyNames(c1));
+console.log(Object.getOwnPropertySymbols(c1));
+let key = Object.getOwnPropertySymbols(c1)[0];
+console.log(c1[key]);
