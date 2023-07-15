00aad7e5182e255ff530bceb5e23045179d94a0a
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-15 00:36:57 -0700

    symbol useকরে hide  করবো

--- 163. Private Data with Sypbole  in JavaScript/app.js
+++ 163. Private Data with Sypbole  in JavaScript/app.js
@@ -1,18 +1,16 @@
 const _radius = Symbol();  // symbol make unique identyfire
 const _name = Symbol();
+const _draw = Symbol();
 class Circle {
     constructor(radius) {
         this[_radius] = radius;
         this[_name] = name;
+
     }
-    draw() {
+    [_draw]() {
         console.log('Drawing.....');
     }
 }
 let c1 = new Circle(2, 'shihab');
 console.log(c1);
 
-console.log(Object.getOwnPropertyNames(c1));
-console.log(Object.getOwnPropertySymbols(c1));
-let key = Object.getOwnPropertySymbols(c1)[0];
-console.log(c1[key]);
