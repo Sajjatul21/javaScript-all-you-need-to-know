b42660912acdf80219869d945e7e7f35d6fceb06
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-16 00:26:11 -0700

    আমাদের এখানে এখন কোন asynchronous
    ঘটি নি আমাদের এখান এখন synchronous programming হয়েছে । asynchronous programming  তখনি হবে যখন আমরা file system, database, rest API, web নিয়ে কাজ করবো ।

---
+++ 177. Asynchronous Programmming is Action in JavaScript/app.js
@@ -0,0 +1,6 @@
+import './lessons/lesson.mjs';
+
+/* আমরা যদি করতে চাই তাহলে আমাদের html file type= 'module' দিতে হবে। আমরা  যেই ফাইল import করতে চাচ্ছি সেই  file নেম
+.mjs দিতে হবে */
+
+{/* <script type="module" src="app.js"></script> */ }
---
+++ 177. Asynchronous Programmming is Action in JavaScript/lessons/lesson.mjs
@@ -0,0 +1,5 @@
+console.log('I am Line One');
+console.log('I am Line Two');
+console.log('I am Line Three');
+console.log('I am Line Four');
+console.log('I am Line Five');
