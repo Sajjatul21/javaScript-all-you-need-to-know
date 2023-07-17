762dfcc09b229fabbc53e05d2ed9ae86a25fc76c
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-17 05:26:49 -0700

    "https://jsonplaceholder.typicode.com" এর পর slash( / ) দিয়ে দিয়েছিলাম এটা হবে না
    res.json( ) আমাদের একটা promise return করে এটা আমরা resolve করবে পরের then  block এ এখানে আমরা  chine করে দিলাম এটা promise এর সুবিদা

--- 188. Implement Fetch API in JavaScript/lessons/lesson.mjs
+++ 188. Implement Fetch API in JavaScript/lessons/lesson.mjs
@@ -1,7 +1,9 @@
-const BASE_URL = "https://jsonplaceholder.typicode.com/";
-fetch(`${BASE_URL}/users/1`)
-    .then(res => {
-        console.log(res.json());
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+
+fetch(`${BASE_URL}/users/2`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
     })
     .catch(e => {
         console.log(e);
