000bf1146337fbd6aa663e4a0413849d28eb754a
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-17 02:44:05 -0700

    we found a undefined arrays

--- 185. Callback not Asynchronous in JavaScript/lessons/lesson.mjs
+++ 185. Callback not Asynchronous in JavaScript/lessons/lesson.mjs
@@ -1,3 +1,13 @@
 let arr = [1, 2, 3, 4];
 let squareArray = arr.map((v) => v * v);
 console.log(squareArray);
+
+
+
+function asyncMap(arr, cb) {
+    return arr.map(v => {
+        setTimeout(cb, 0);
+    });
+}
+let qbArr = asyncMap(arr, v => v * v * v);
+console.log(qbArr);
