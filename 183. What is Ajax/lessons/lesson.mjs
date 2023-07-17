27bc6eff03aeea1f88a78d270d12a1d2a9503b3d
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-16 21:22:42 -0700

    Asynchronous JavaScript And XML.

---
+++ 183. What is Ajax/lessons/lesson.mjs
@@ -0,0 +1,8 @@
+/*  
+    AJAX -> Asynchronous JavaScript And XML.
+
+    AJAX হলো  web api যার মাধ্যমে আমরা ্আমদের server বা অন্য কেনা  server/database থেকে  url এর মাধ্যমে data transfer করতে পারি। 
+
+    এখন AJAX এর পরিবর্তে Json ব্যবহার হয়।
+    Json -> javaScript object Notation
+*/
