bec4368bfac731f57dda273a546f0af710f2dc5b
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-16 21:56:42 -0700

    আমরা যদি আগের মতো করে করতাম তাহলে আমরা একবার ই request পাঠাতে পারতাম । তাই এখন আমরা callback use করলাম

--- 184. Ajax with Callback in JavaScript/lessons/lesson.mjs
+++ 184. Ajax with Callback in JavaScript/lessons/lesson.mjs
@@ -1,4 +1,4 @@
-const xhr = new XMLHttpRequest();
+/* const xhr = new XMLHttpRequest();
 xhr.open('get', 'https://jsonplaceholder.typicode.com/users');
 
 xhr.onreadystatechange = function (event) {
@@ -12,4 +12,32 @@ xhr.onreadystatechange = function (event) {
         }
     }
 };
-xhr.send();
+xhr.send(); */
+
+function getRequest(url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('get', url);
+
+    xhr.onreadystatechange = function (event) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+
+                let response = JSON.parse(xhr.response);
+                callback(null, response);
+            }
+            else {
+                callback(xhr.status, null);
+            }
+        }
+    };
+    xhr.send();
+
+}
+getRequest('https://jsonplaceholder.typicode.com/users', (err, res) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log(res);
+    }
+});
