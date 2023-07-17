c5988e57465900b0146b1ce075795b8e9d255694
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-17 05:22:10 -0700

    আমরা যখন promise টাকে json এর convert করি তখন সে আমাদের আরেকটা promise দেয় যেটা আগে থেকেই  fullfil করা

--- 188. Implement Fetch API in JavaScript/lessons/lesson.mjs
+++ 188. Implement Fetch API in JavaScript/lessons/lesson.mjs
@@ -1,7 +1,7 @@
 const BASE_URL = "https://jsonplaceholder.typicode.com/";
 fetch(`${BASE_URL}/users/1`)
     .then(res => {
-        console.log(res);
+        console.log(res.json());
     })
     .catch(e => {
         console.log(e);
