a7e2306a1860e1cf82c78a46bb9b3895ea798cec
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-16 21:39:35 -0700

    xml http request  এর মাধ্যমে ডাটা লোড করে আনলাম

---
+++ 184. Ajax with Callback in JavaScript/lessons/lesson.mjs
@@ -0,0 +1,14 @@
+const xhr = new XMLHttpRequest();
+xhr.open('get', 'https://jsonplaceholder.typicode.com/users');
+
+xhr.onreadystatechange = function (event) {
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+            console.log(xhr.response);
+        }
+        else {
+            console.log(xhr.status);
+        }
+    }
+};
+xhr.send();
