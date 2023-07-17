8511cc5e273945fe783d082d12a90c88dfa2cf11
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-17 09:39:56 -0700

    promise alll

--- 189. Promise API in JavaScript/lessons/lesson.mjs
+++ 189. Promise API in JavaScript/lessons/lesson.mjs
@@ -6,11 +6,21 @@ delay(2).then(() => console.log('2 second delay'));
 delay(5).then(() => console.log('5 second delay')); */
 
 
-let p1 = Promise.resolve('test');
+/* let p1 = Promise.resolve('test');
 // console.log(p1);
 p1.then((v) => {
     console.log(v);
 });
 
 let p2 = Promise.reject('REJECT');
-p2.catch((e) => console.log(e));
+p2.catch((e) => console.log(e)); */
+
+let p1 = Promise.resolve('One');
+let p2 = Promise.resolve('Two');
+let p3 = Promise.resolve('Three');
+
+let promiseArr = [p1, p2, p3];     /* Promise.all argument আকারে array গ্রহন করে তাই আমরা promise গুলাকে array এর মধ্যে রাখলাম */
+Promise.all(promiseArr)
+    .then(arr => {
+        console.log(arr);
+    });
