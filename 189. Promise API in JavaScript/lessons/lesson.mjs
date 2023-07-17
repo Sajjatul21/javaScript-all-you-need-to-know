f699e4fc79387f7d8525637352830bd4a5e3a60d
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-17 09:31:11 -0700

    আমরা চাইলে যেই কোন জায়গায় resolve promise make করতে পারি।  Promise.resolve এই মেথটা instand একটা resolve promise make করবে এবং সাথে সাথে fullfil করবে ফেলবে

--- 189. Promise API in JavaScript/lessons/lesson.mjs
+++ 189. Promise API in JavaScript/lessons/lesson.mjs
@@ -1,6 +1,10 @@
-const delay = s => new Promise((resolve) => setTimeout(resolve, s * 1000));
+/* const delay = s => new Promise((resolve) => setTimeout(resolve, s * 1000));
 delay(2).then(() => console.log('2 second delay'));
 delay(1).then(() => console.log('1 second delay'));
 delay(3).then(() => console.log('3 second delay'));
 delay(2).then(() => console.log('2 second delay'));
-delay(5).then(() => console.log('5 second delay'));
+delay(5).then(() => console.log('5 second delay')); */
+
+
+let p1 = Promise.resolve('test');
+console.log(p1);
