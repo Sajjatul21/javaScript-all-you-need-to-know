45e3f73592ab4a61ca4265d2fa82497f6ed4aa52
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-17 05:15:46 -0700

    get value of promise we use then and catch

--- 188. Implement Fetch API in JavaScript/lessons/lesson.mjs
+++ 188. Implement Fetch API in JavaScript/lessons/lesson.mjs
@@ -1,3 +1,8 @@
 const BASE_URL = "https://jsonplaceholder.typicode.com/";
-let res = fetch(`${BASE_URL}/users/1`);
-console.log(res);
+fetch(`${BASE_URL}/users/1`)
+    .then(res => {
+        console.log(res);
+    })
+    .catch(e => {
+        console.log(e);
+    });
