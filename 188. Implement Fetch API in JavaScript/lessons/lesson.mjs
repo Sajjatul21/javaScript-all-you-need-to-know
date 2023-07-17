3ac0ad339bb58b583b879f221ef7685f24769f16
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-17 05:33:49 -0700

    আমরা একটার ভিতরে আরেকটা promise দিলাম ।  data এর ভিতরে আরেকটা promise সেখান থেকে আরেকটা promise return করলাম । তা আমরা  output এর দেখতে পেলাম
    এরা একে অপরের child তাই একটার আগে আরেকটা আসছে না

--- 188. Implement Fetch API in JavaScript/lessons/lesson.mjs
+++ 188. Implement Fetch API in JavaScript/lessons/lesson.mjs
@@ -4,6 +4,14 @@ fetch(`${BASE_URL}/users/2`)
     .then(res => res.json())
     .then(data => {
         console.log(data);
+        return Promise.resolve('Something');
+    })
+    .then(str => {
+        console.log(str);
+        return Promise.resolve('another promise');
+    })
+    .then(another => {
+        console.log(another);
     })
     .catch(e => {
         console.log(e);
