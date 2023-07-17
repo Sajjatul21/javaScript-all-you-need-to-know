67a4e227058b25df088801b453dae70aab407004
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-17 03:34:55 -0700

    create another pormise

--- 187. Introduction to Promise in JavaScript/lessons/lesson.mjs
+++ 187. Introduction to Promise in JavaScript/lessons/lesson.mjs
@@ -5,4 +5,10 @@
 let p1 = new Promise((resolve, reject) => {
     setTimeout(resolve, 5000, 'One');
 });
+
+let p2 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 3000, 'Two');
+});
+
 console.log(p1);
+console.log(p2);
