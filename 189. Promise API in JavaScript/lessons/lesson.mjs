0761354678cec74ca71dd098cb99d3572d62c77c
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-17 09:54:31 -0700

    আউটপুটে সব গুলা ফলাফল আমরা একসাথে দেখতে পাবো
    তার মানে যখন সব গুলা promise resolve হবে তখন আমরা আউটপুট পাবো. এর মদ্দে একটাও যদি এরর আসে তখন catch এ চলে যাবে তখন আমরা এরর পাবো. যতক্ষন না promise resolve হবে ততক্ষণ আমরা আউটপুট পাবো না

--- 189. Promise API in JavaScript/lessons/lesson.mjs
+++ 189. Promise API in JavaScript/lessons/lesson.mjs
@@ -15,11 +15,30 @@ p1.then((v) => {
 let p2 = Promise.reject('REJECT');
 p2.catch((e) => console.log(e)); */
 
+/* 
 let p1 = Promise.resolve('One');
 let p2 = Promise.resolve('Two');
 let p3 = Promise.resolve('Three');
 
-let promiseArr = [p1, p2, p3];     /* Promise.all argument আকারে array গ্রহন করে তাই আমরা promise গুলাকে array এর মধ্যে রাখলাম */
+let promiseArr = [p1, p2, p3];     // Promise.all argument আকারে array গ্রহন করে তাই আমরা promise গুলাকে array এর মধ্যে রাখলাম 
+Promise.all(promiseArr)
+    .then(arr => {
+        console.log(arr);
+    }); */
+
+
+let p1 = new Promise(resolve => {
+    setTimeout(resolve, 5000, 'One');
+});
+
+let p2 = new Promise(resolve => {
+    setTimeout(resolve, 3000, 'Two');
+});
+let p3 = new Promise(resolve => {
+    setTimeout(resolve, 4000, 'Three');
+});
+
+let promiseArr = [p1, p2, p3];
 Promise.all(promiseArr)
     .then(arr => {
         console.log(arr);
