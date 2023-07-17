4754cf7a13d49295f07db67950127c1712f3944f
Sajjatul21 <76829263+Sajjatul21@users.noreply.github.com>
2023-07-17 03:55:39 -0700

    promise use case

--- 187. Introduction to Promise in JavaScript/lessons/lesson.mjs
+++ 187. Introduction to Promise in JavaScript/lessons/lesson.mjs
@@ -1,7 +1,7 @@
 /* promise হচ্ছে  js এর constructor function । যেই function টা ব্যবহার করে আমরা একটা promise object তৈরি করে থাকি ।
    যে function/ constructor make করার জন্য আমাদের একটা argument আকারে callback function  দিতে হয়, এবং callBack function
    এর ভিতরে 2 টা টার্ম থাকতে পারে resolve অথবা reject ।
-*/
+*//* 
 let p1 = new Promise((resolve, reject) => {
     setTimeout(resolve, 5000, 'One');
 });
@@ -12,10 +12,27 @@ let p2 = new Promise((resolve, reject) => {
 
 p1.then((v) => {
     console.log(v);
-})/* .catch((e)=>{
-    console.log(e)
-}) */
+}); // .catch((e)=>{
+//     console.log(e)
+// }) 
 
 p1.then((v) => {
     console.log(v);
-});
+}); */
+
+// promise use case 
+
+function getIPhone(isPassed) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (isPassed) {
+                resolve('I have Got an Iphone');
+            }
+            else {
+                reject('You have Failed');
+            }
+        }, 2000);
+    });
+}
+// console.log(getIPhone(true));
+console.log(getIPhone(false));
